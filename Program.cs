d7323b5019a5c93fdff8ac4499820fc12fc0c491
Vitoria <vitoriahellen3004@gmail.com>
2020-10-03 22:28:54 -0300

    Calcule o montante final de um investimento a juros simples.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace jurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capital; 
+            double juros; 
+            double tempo; 
+            double resultadojuros;
+            double montante; 
+
+            Console.WriteLine("Valores: \n");
+            Console.Write("Capital: ");
+            capital = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Juros: ");
+            juros = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Tempo: ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+            resultadojuros = capital * (juros/100) * tempo;
+            Console.Write($"\nJuros: {resultadojuros:N2}\n");
+            montante = capital + resultadojuros; 
+            Console.Write($"Montante: {montante:N2}");
+
+
+        }
+    }
+}
